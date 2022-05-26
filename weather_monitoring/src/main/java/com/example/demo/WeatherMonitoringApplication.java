00831fafd23a31e7b844d0f0c7d81e5e74ead683
sk_shahid_ali <sk.shahid.ali1995@gmail.com>
2022-05-26 18:34:23 +0530

    added demo controller

--- weather_monitoring/src/main/java/com/example/demo/WeatherMonitoringApplication.java
+++ weather_monitoring/src/main/java/com/example/demo/WeatherMonitoringApplication.java
@@ -2,12 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.weather"})
 @SpringBootApplication
 public class WeatherMonitoringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherMonitoringApplication.class, args);
+		System.out.println("Application started");
 	}
 
 }
